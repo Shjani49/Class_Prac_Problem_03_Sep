f1871e889dd08c1cdb7710c0c73807b67d757865
James Grieve <jamesonrgrieve@gmail.com>
2020-08-24 10:54:40 -0600

    Null coalescing operator;

--- CSharpOOP/Program.cs
+++ CSharpOOP/Program.cs
@@ -6,8 +6,17 @@ namespace CSharpOOP
     {
         static void Main(string[] args)
         {
-            Console.Write("Please enter a string: ");
-            Console.WriteLine(Console.ReadLine().Trim().Contains(' ') ? "Multiple words." : "One word.");
+            string inputOne = null, inputTwo = null, inputThree = "Yes";
+
+            // Null coalescing operator will use the left hand side if it is not null, otherwise the right side.
+            Console.WriteLine(inputOne??"The value was null.");
+
+            // It's kind of the same as:
+            Console.WriteLine(inputOne != null ? inputOne : "The value was null.");
+
+
+            // The can be strung together as well:
+            Console.WriteLine(inputOne??inputTwo??inputThree);
         }
     }
 }
