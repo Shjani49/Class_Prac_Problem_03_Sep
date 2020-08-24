2203bc4ca4b8419c48f42bc9a6da8082a7374afc
James Grieve <jamesonrgrieve@gmail.com>
2020-08-24 11:43:59 -0600

    Added overload to sleep; Added ceiling to energy; Added independent task;

--- CSharpOOP/Program.cs
+++ CSharpOOP/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace CSharpOOP
 {
@@ -11,6 +12,12 @@ namespace CSharpOOP
             // Create methods in the student that are accessible, for do homework and sleep.
             // Do homework will lower the student's energy level by 25. Sleep will set the energy level to 100.
             // When a student is created, ensure their energy level starts at 100.
+
+            // Overload the sleep method to allow a value of hours. Increase energy by 10 per hour slept.
+            // Add a ceiling of 100 to sleep.
+
+            // Task:
+            // Add a play games method that will decrease energy by 15.
 
             Student myStudent = new Student();
 
--- CSharpOOP/Student.cs
+++ CSharpOOP/Student.cs
@@ -11,14 +11,53 @@ namespace CSharpOOP
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
+        // We have to specify the backing variable for EnergyLevel because we are doing some validation with the value. 
+        private int _energyLevel;
+        private int EnergyLevel { 
+            get
+            {
+                return _energyLevel;
+            } 
+            set
+            {
+                // If the incoming value is over 100, clamp it to 100.
+                if (value > 100)
+                {
+                    _energyLevel = 100;
+                }
+                else
+                {
+                    _energyLevel = value;
+                }
+            }
+        }
+
         // Private properties can only be set in this class.
         private DateTime DateOfBirth { get; set; }
-
 
         public Student()
         {
             DateOfBirth = DateTime.Now;
+            EnergyLevel = 100;
         }
+
+        public void DoHomework()
+        {
+            EnergyLevel -= 25;
+            // Same as EnergyLevel = EnergyLevel - 25;
+        }
+
+        public void Sleep()
+        {
+            EnergyLevel = 100;
+        }
+
+        public void Sleep(int hours)
+        {
+            EnergyLevel += hours * 10;
+        }
+
+        
 
 
 
