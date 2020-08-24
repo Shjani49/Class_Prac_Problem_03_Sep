8bb308c0b645cf6a4527526ec40c6f6e29550d22
James Grieve <jamesonrgrieve@gmail.com>
2020-08-24 11:15:41 -0600

    In class practice spec;

--- CSharpOOP/Program.cs
+++ CSharpOOP/Program.cs
@@ -6,32 +6,14 @@ namespace CSharpOOP
     {
         static void Main(string[] args)
         {
-            // Generate a class "Student" with a string for FirstName, LastName and a int for StudentID.
+            // Modify the student class to have a property for energy level.
+            // The energy level should not be accessible from outside the student.
+            // Create methods in the student that are accessible, for do homework and sleep.
+            // Do homework will lower the student's energy level by 25. Sleep will set the energy level to 100.
 
             Student myStudent = new Student();
 
-            WriteSomeStuff();
-            WriteSomeStuff();
-            WriteSomeStuff("Yes!");
-            WriteSomeStuff("No!");
-            WriteSomeStuff("Maybe!");
-            WriteSomeStuff("Yes", "No");
-        }
 
-        // Multiple versions of a method can be declared, as long as their parameter type lists differ.
-        // This is called method "overloading".
-        // The names of the parameters do not matter, they must be different types.
-        static void WriteSomeStuff()
-        {
-            Console.WriteLine("Some stuff is being written!");
-        }
-        static void WriteSomeStuff(string stuff)
-        {
-            Console.WriteLine(stuff);
-        }
-        static void WriteSomeStuff(string someStuff, string someOtherStuff)
-        {
-            Console.WriteLine(someStuff + someOtherStuff);
         }
     }
 }
