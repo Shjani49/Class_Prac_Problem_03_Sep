470ea555761cae2b12a4031a71652c9a23c1e24f
James Grieve <jamesonrgrieve@gmail.com>
2020-08-24 10:46:55 -0600

    Practice round 2 solution;

--- CSharpOOP/Program.cs
+++ CSharpOOP/Program.cs
@@ -6,18 +6,8 @@ namespace CSharpOOP
     {
         static void Main(string[] args)
         {
-            string input, output;
-            // In-Class Practice Round 2:
-
-            // Try to do it without any variables at all (remove the line above these comments).
-            // Two lines (prompt and output).
-
             Console.Write("Please enter a string: ");
-            input = Console.ReadLine();
-
-            output = input.Trim().Contains(' ') ? "Multiple words." : "One word.";
-
-            Console.WriteLine(output);
+            Console.WriteLine(Console.ReadLine().Trim().Contains(' ') ? "Multiple words." : "One word.");
         }
     }
 }
