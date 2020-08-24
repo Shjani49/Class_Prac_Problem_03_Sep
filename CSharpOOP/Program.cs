b0019eeeedf8a4149a66ad3e658da0b98c32df21
James Grieve <jamesonrgrieve@gmail.com>
2020-08-24 11:17:58 -0600

    Added one spec point;

--- CSharpOOP/Program.cs
+++ CSharpOOP/Program.cs
@@ -10,6 +10,7 @@ namespace CSharpOOP
             // The energy level should not be accessible from outside the student.
             // Create methods in the student that are accessible, for do homework and sleep.
             // Do homework will lower the student's energy level by 25. Sleep will set the energy level to 100.
+            // When a student is created, ensure their energy level starts at 100.
 
             Student myStudent = new Student();
 
